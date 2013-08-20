4c2113147b0732c3fd0ddae261e9174a3c398faa
Larry Xu <larry@larryxu.com>
2013-08-20 01:25:12 -0700

    Built profile layout

---
+++ index.php
@@ -0,0 +1,91 @@
+
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Larry Xu</title>
+
+  
+  <link rel="stylesheet" href="stylesheets/app.css">
+  <link  rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css">
+  <link rel="stylesheet" href="stylesheets/main.css">
+  
+
+  <script src="javascripts/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+
+  <div class="row">
+    <div id="profile-bar" class="large-3 columns">
+      <div class="row">
+        <div class="small-5 columns">
+          <img id="profile-picture" src="http://placehold.it/150x190">
+        </div>
+        <div id="profile-info" class="small-7 columns">
+          <h2>Larry Xu</h2>
+          <p>Web and Graphic Designer</p>
+          <div class="social-container">
+            <a id="linkedin-icon" class="social-icon" href="#"><i class="icon-linkedin-sign"></i></a>
+            <a id="facebook-icon" class="social-icon" href="#"><i class="icon-facebook-sign"></i></a>
+            <a id="google-plus-icon" class="social-icon" href="#"><i class="icon-google-plus-sign"></i></a>
+            <a id="flickr-icon" class="social-icon" href="#"><i class="icon-flickr"></i></a>
+          </div>
+          <div class="contact-info">
+            <i class="icon-envelope"></i> : <a href="#">larry@larryxu.com</a>
+          </div>
+          <div class="contact-info">
+            <i class="icon-phone contact-icon"></i> : 408.334.3179
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'javascripts/vendor/zepto' : 'javascripts/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="javascripts/foundation/foundation.js"></script>
+  
+  <script src="javascripts/foundation/foundation.abide.js"></script>
+  
+  <script src="javascripts/foundation/foundation.alerts.js"></script>
+  
+  <script src="javascripts/foundation/foundation.clearing.js"></script>
+  
+  <script src="javascripts/foundation/foundation.cookie.js"></script>
+  
+  <script src="javascripts/foundation/foundation.dropdown.js"></script>
+  
+  <script src="javascripts/foundation/foundation.forms.js"></script>
+  
+  <script src="javascripts/foundation/foundation.interchange.js"></script>
+  
+  <script src="javascripts/foundation/foundation.joyride.js"></script>
+  
+  <script src="javascripts/foundation/foundation.magellan.js"></script>
+  
+  <script src="javascripts/foundation/foundation.orbit.js"></script>
+  
+  <script src="javascripts/foundation/foundation.placeholder.js"></script>
+  
+  <script src="javascripts/foundation/foundation.reveal.js"></script>
+  
+  <script src="javascripts/foundation/foundation.section.js"></script>
+  
+  <script src="javascripts/foundation/foundation.tooltips.js"></script>
+  
+  <script src="javascripts/foundation/foundation.topbar.js"></script>
+  
+  
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
