e70074892594b5a293de4d742041b6b304e4bce2
Larry Xu <larry@larryxu.com>
2013-09-03 01:40:32 -0700

    Updated projects

--- index.php
+++ index.php
@@ -65,17 +65,18 @@
             <div class="large-8 columns">
               <p>I am a Berkeley-based web and graphic designer.</p>
               <p>My passion is to develop innovative web applications and design beautiful promotional graphics. I have had experience crafting designs and websites for various clients, organizations, and companies, as well as completing independent projects and competitions. You can check out samples of my work below, as well as my <a href="larryxu_resume.pdf">resume</a> for more detailed information.</p>
+              <p class="show-for-xlarge">I am proficient in current and new web technologies like HTML5, CSS3, JavaScript, and PHP as well as experienced with various design tools like Adobe's Creative Suite. I also love photography and am always on the lookout for my next great shot. With all of these skills combined, I am poised to help craft your next big marketing campaign or website idea. Feel free to contact me, as I am available for hire.</p>
             </div>
             <div class="large-4 columns hide-for-medium-down latest-projects">
               <h5>Latest Projects</h5>
               <ul class="no-bullet">
                 <li>
-                  <img src="images/web/la_pluma_small.png">
-                  <h6>La Pluma</h6>
+                  <img src="images/web/verdadera_small.png">
+                  <h6>Verdadera</h6>
                 </li>
                 <li>
-                  <img src="images/graphics/ddp_small.png">
-                  <h6>Xtreme Adventure Travel</h6>
+                  <img src="images/graphics/stephen_skeel_small.png">
+                  <h6>Stephen Skeel</h6>
                 </li>
               </ul>
             </div>
@@ -92,6 +93,10 @@
           <hr>
           <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3 portfolio-grid clearing-thumbs" data-clearing>
             <li>
+              <a class="thumbnail" href="images/web/verdadera.png"><img src="images/web/verdadera_small.png" data-caption="<strong>Verdadera:</strong> Verdadera literary magazine partnered with MV Website Development club to develop their online public archival and submission system for students to post creative works and media. As head of this project, I worked with members in drafting designs and features for the final website."></a>
+              <h6>Verdadera</h6>
+            </li>
+            <li>
               <a class="thumbnail" href="images/web/la_pluma.png"><img src="images/web/la_pluma_small.png" data-caption="<strong>La Pluma:</strong> La Pluma literary magazine commissioned me to develop their online public archival and submission system for students to post creative works and media."></a>
               <h6>La Pluma</h6>
             </li>
@@ -102,6 +107,26 @@
             <li>
               <a class="thumbnail" href="images/web/mvfbla.png"><img src="images/web/mvfbla_small.png" data-caption="<strong>MV FBLA:</strong> As Director of Technology of my local FBLA chapter, I created this website to enhance MVFBLA’s online presence. Features include a news feed, photo gallery, and competitive information."></a>
               <h6>MV FBLA</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/web/realvu.PNG"><img src="images/web/realvu_small.png" data-caption="<strong>RealVu:</strong> During my internship at Polycom Inc., I designed and developed an online interviewing application to allow their HR department to schedule live video interviews with potential hires."></a>
+              <h6>RealVu</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/web/teeno.png"><img src="images/web/teeno_small.png" data-caption="<strong>Teeno:</strong> This website was a submission for the 2012 FBLA Web Site Design national competition, and includes social media integration as well as a responsive design layout. I received 8th place at the national level."></a>
+              <h6>Teeno</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/web/ae.png"><img src="images/web/ae_small.png" data-caption="<strong>American Enterprise:</strong> As part of MVFBLA's annual American Enterprise Project, I created this website to keep members updated on the project's events."></a>
+              <h6>American Enterprise</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/web/mvnhs.png"><img src="images/web/mvnhs_small.png" data-caption="<strong>MV NHS:</strong> As Webmaster of Monta Vista NHS, I created a system that allows members to sign up for volunteer events as well as track and receive volunteer hours."></a>
+              <h6>MV NHS</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/web/mvmatshack.png"><img src="images/web/mvmatshack_small.png" data-caption="<strong>MV MatShack:</strong> This e-commerce website was a submission for the 2011 FBLA E-business national competition, and includes a custom shopping cart and checkout system. I received 3rd place at the national level."></a>
+              <h6>MV MatShack</h6>
             </li>
           </ul>
         </div>
@@ -116,6 +141,10 @@
           <hr>
           <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3 portfolio-grid clearing-thumbs" data-clearing>
             <li>
+              <a class="thumbnail" href="images/graphics/stephen_skeel.png"><img src="images/graphics/stephen_skeel_small.png" data-caption="<strong>Stephen Skeel:</strong> Through various drafts and client interactions, I designed the brand logo for film director Stephen Skeel."></a>
+              <h6>Stephen Skeel</h6>
+            </li>
+            <li>
               <a class="thumbnail" href="images/graphics/ddp.png"><img src="images/graphics/ddp_small.png" data-caption="<strong>Xtreme Adventure Travel:</strong> For the 2013 FBLA Digital Design and Promotion competition, I created a brand identity, including logo and promotional package, for a travel tour company."></a>
               <h6>Xtreme Adventure Travel</h6>
             </li>
@@ -126,6 +155,22 @@
             <li>
               <a class="thumbnail" href="images/graphics/mvfbla_graphics.png"><img src="images/graphics/mvfbla_graphics_small.png" data-caption="<strong>MV FBLA Promotion:</strong> I crafted MVFBLA's new promotional campaign including logo, banners, and fliers to help increase membership and club awareness."></a>
               <h6>MV FBLA Promotion</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/graphics/nikitas.png"><img src="images/graphics/nikitas_small.png" data-caption="<strong>FBLA National Campaign:</strong> For the 2012 FBLA National Leadership Conference, I designed the campaign and promotional materials for FBLA National President Nikitas Kanellakopoulos. He won."></a>
+              <h6>FBLA National Campaign</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/graphics/brochures.png"><img src="images/graphics/brochures_small.png" data-caption="<strong>FBLA State Campaigns:</strong> For the California FBLA State Leadership Conference, I created these brochures and business cards for two candidates running for state officer positions."></a>
+              <h6>FBLA State Campaigns</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/graphics/projects.png"><img src="images/graphics/projects_small.png" data-caption="<strong>FBLA Project Reports:</strong> All of MVFBLA's 2012 annual project reports were designed and formatted by me or with my consultation."></a>
+              <h6>FBLA Project Reports</h6>
+            </li>
+            <li>
+              <a class="thumbnail" href="images/graphics/it_graphic.png"><img src="images/graphics/it_graphic_small.png" data-caption="<strong>Technology Workshop:</strong> In order to help educate FBLA's tech competitors about technology and its uses, I created this infographic and presented it to students at a workshop."></a>
+              <h6>Technology Workshop</h6>
             </li>
           </ul>
         </div>
